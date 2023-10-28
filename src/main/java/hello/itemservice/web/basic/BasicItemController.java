57d32c85034aa4b4412ced0edc2636f4e1e93b8e
le2sky <leehaneul990623@gmail.com>
2023-10-28 22:39:22 +0900

    style : 불필요한 공백 제거

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -92,14 +92,12 @@ public class BasicItemController {
         return "basic/editForm";
     }
 
-
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
         itemRepository.update(itemId, item);
 
         return "redirect:/basic/items/{itemId}";
     }
-
 
     @PostConstruct
     public void init() {
