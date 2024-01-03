63cc30c6984aaf3c74f0af6d897fdef4a0f073e2
le2sky <leehaneul990623@gmail.com>
2024-01-03 19:18:40 +0900

    refactor : 상품 등록 기능 PRG 패턴 적용

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -2,13 +2,17 @@ package hello.itemservice.web.basic;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.PostConstruct;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/basic/items")
@@ -38,10 +42,10 @@ public class BasicItemController {
 
     //    @PostMapping("/add")
     public String addItemV1(
-            @RequestParam String itemName,
-            @RequestParam int price,
-            @RequestParam Integer quantity,
-            Model model
+        @RequestParam String itemName,
+        @RequestParam int price,
+        @RequestParam Integer quantity,
+        Model model
     ) {
         Item item = new Item();
         item.setItemName(itemName);
@@ -57,7 +61,7 @@ public class BasicItemController {
 
     //    @PostMapping("/add")
     public String addItemV2(
-            @ModelAttribute("item") Item item
+        @ModelAttribute("item") Item item
     ) {
         itemRepository.save(item);
 
@@ -68,19 +72,26 @@ public class BasicItemController {
 
     //    @PostMapping("/add")
     public String addItemV3(
-            // 클래스 명으로 attribute에 등록 Item -> item
-            @ModelAttribute Item item
+        // 클래스 명으로 attribute에 등록 Item -> item
+        @ModelAttribute Item item
     ) {
         itemRepository.save(item);
 
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    //    @PostMapping("/add")
     public String addItemV4(Item item) {
         itemRepository.save(item);
 
         return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
